adaa0beed53728e981b4a6e8a553833a75a64510
Sriram Radhakrishna <83068519+SR42-dev@users.noreply.github.com>
2022-03-30 20:37:25 +0530

    Add files via upload

---
+++ 3-candlestick-approach/setup.py
@@ -0,0 +1,11 @@
+'''
+Setup file to be called by optoBot_v1.0.py
+Stuff to implement :
+    - Add front-end
+    - Boot up browser
+    - Set to fullscreen
+    - Pre-code all clicks and mouse moves
+    - Log in to binomo (pre-login/use .json file method)
+    - Set to 3 candlesticks mode (pre-done/manual)
+    - Add variable to json file to determine demo mode/real mode
+'''
